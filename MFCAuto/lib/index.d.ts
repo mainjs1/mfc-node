7fc4819bcf49c732eb320b9dcda2af4491ee3f12
Damianonymous <zetixpe32@gmail.com>
2018-05-18 04:28:07 +0200

    Delete index.d.ts

--- MFCAuto/lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./Client";
-export * from "./Constants";
-export * from "./Model";
-export * from "./Packet";
-export * from "./sMessages";
-export { LogLevel, setLogLevel, log, logWithLevel, applyMixins } from "./Utils";
